5b128ea04e10586eb88280c74867aac70116ba25
Nitin Sahu <sahunitin970@gmail.com>
2024-05-28 11:37:44 +0530

    floor added to the scene - (Plane)

--- main.js
+++ main.js
@@ -34,9 +34,10 @@ let sunLight = new THREE.DirectionalLight(0xdddddd, 1.0); // color intensity
 sunLight.position.y = 15;
 scene.add(sunLight);
 //Object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.BoxGeometry(1, 1, 1, 20, 20, 20);
 const material = new THREE.MeshBasicMaterial({
     color: 0x111111,
+    wireframe: true,
 });
 const cube = new THREE.Mesh(geometry, material); // create cute with geometry andd material
 scene.add(cube); // add cube to scene
@@ -44,6 +45,14 @@ scene.add(cube); // add cube to scene
 // Controls
 // EventListener for, when we press the keys
 window.addEventListener("keydown", onKeyDown, false);
+// create the floor plane.
+const planeGeometry = new THREE.PlaneGeometry(50, 50);
+const planeMaterial = new THREE.MeshBasicMaterial({
+    color: "green",
+    side: THREE.DoubleSide
+});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(plane);
 // function when key is pressed, execute the function
 function onKeyDown(event) {
     switch (event.keyCode) {
@@ -58,15 +67,15 @@ function onKeyDown(event) {
             break;
         case 40: // bottom
             cube.translateY(0.05);
-            break;  
+            break;
     }
 }
 // Animate renderer loop
 const Animate = () => {
     requestAnimationFrame(Animate);
     // Update
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
     // Render
     renderer.render(scene, camera);
 };
